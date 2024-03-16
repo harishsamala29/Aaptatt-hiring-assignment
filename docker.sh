aaf28fc9b07fafe3f61d471b9b0e83b2055d1773
Harish Reddy Samala <161340680+harishsamala29@users.noreply.github.com>
2024-03-16 18:38:44 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,3 @@
+  GNU nano 6.2                                                                                       docker.sh                                                                                                 
+#/bin/bash
+docker login -u harish78 -p dckr_pat_n9mhdp_JGyJ97R7B8nFeeqqPF6M
