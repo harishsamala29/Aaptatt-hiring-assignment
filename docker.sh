3278403b18e5a30ebd04cc292a4fdf08c13df6bf
Harish Reddy Samala <161340680+harishsamala29@users.noreply.github.com>
2024-03-16 18:52:22 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
                                                                                               
 #/bin/bash
-docker login -u harish78 -p dckr_pat_n9mhdp_JGyJ97R7B8nFeeqqPF6M
+docker login -u harish78 -p dckr_pat_n9mhdp_JGyJ97R7B8nFeeqqPF6MM
